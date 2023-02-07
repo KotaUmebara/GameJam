0f3d5907156e4e009214d5770af194d2435f027e
Umebara <Nakoichi39@outlook.jp>
2023-02-07 11:17:38 +0900

    スクリプトの変更

--- Assets/Umebara/ScriptFile/RandomGoal.cs
+++ Assets/Umebara/ScriptFile/RandomGoal.cs
@@ -1,29 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RandomGoal : MonoBehaviour
 {
     // 生成するプレハブ格納用
     public GameObject PrefabBrige;
     public GameObject PrefabCy;
-    //一度だけ実行
+    //正誤判定
     private bool first;
+    //Playerの格納
+    [SerializeField] GameObject Player;
     void Start()
     {
         first = false;
+        //Playerの取得
+        Player = GameObject.Find("Player");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //初めの一回だけプレハブを生成
-        if (first == false)
+        // transformを取得
+        Transform PlayerTransform = this.Player.transform;
+        // 座標を取得
+        Vector3 pos = PlayerTransform.position;
+        //筒と橋を設置
+        if (pos.z >= 50.0f && first == false)
         {
-            // プレハブの位置をランダムで設定
-            float x = Random.Range(-5.0f, 5.0f);
-            Vector3 pos1 = new Vector3(x, -4.42f, 54.75f);
-            Vector3 pos2 = new Vector3(x, -7.05f, 60.4f);
+            //Playerの位置に合わせて
+            Vector3 pos1 = new Vector3(pos.x, -4.42f, 54.75f);
+            Vector3 pos2 = new Vector3(pos.x, -7.05f, 60.4f);
 
             // プレハブを生成
             Instantiate(PrefabBrige, pos1, Quaternion.identity);
