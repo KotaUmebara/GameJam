bd187a34657c99e215d050f9457d2a94f74556e3
DESKTOP-ATM897C\kazuy <hmstmo1049@icloud.com>
2023-02-07 22:21:53 +0900

    奈落に落ちた時に処理を追加しました。

--- Assets/kazuya/Script/BallController.cs
+++ Assets/kazuya/Script/BallController.cs
@@ -6,16 +6,16 @@ public class BallController : MonoBehaviour
 {
     public Rigidbody rb;
     private bool is_jumpable;
-   void FixedUpdate () 
+    void FixedUpdate()
     {
-        Rigidbody rb = this.GetComponent<Rigidbody> ();  // rigidbodyを取得
-        Vector3 force = new Vector3 (0.0f,0.0f,10.0f);    // 力を設定
-        rb.AddForce (force);  // 力を加える
+        Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
+        Vector3 force = new Vector3(0.0f, 0.0f, 10.0f);    // 力を設定
+        rb.AddForce(force);  // 力を加える
     }
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody> ();
+        rb = GetComponent<Rigidbody>();
         is_jumpable = false;
     }
 
@@ -25,7 +25,7 @@ public class BallController : MonoBehaviour
         //右に動かすプログラム
         if (Input.GetKey(KeyCode.A) == true)
         {
-            transform.Translate(new Vector3(0,0, -0.05f));
+            transform.Translate(new Vector3(0, 0, -0.05f));
         }
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
@@ -41,10 +41,11 @@ public class BallController : MonoBehaviour
             transform.Translate(new Vector3(0f, 0, 0.05f));
         }
         //ジャンプするプログラム
-        if (Input.GetKeyDown(KeyCode.Space)&& is_jumpable == true)
+        if (Input.GetKeyDown(KeyCode.Space) && is_jumpable == true)
         {
-            rb.AddForce(new Vector3(0, 900, 0), ForceMode.Force);
+           rb.AddForce(new Vector3(0, 900, 0), ForceMode.Force);
         }
+
         if (Input.GetKeyDown(KeyCode.S ) == true)
         {
             rb.AddForce(new Vector3(0, -600, 0), ForceMode.Force);
@@ -62,7 +63,8 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        is_jumpable =false;
+        is_jumpable = false;
     }
+ 
 }
 
