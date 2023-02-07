45f5a2021a291f25b2fa83bec03f0f096c555303
DESKTOP-ATM897C\kazuy <hmstmo1049@icloud.com>
2023-02-07 12:20:00 +0900

    CameraControllerを追加しました。

--- Assets/kazuya/CamaraController.cs
+++ Assets/kazuya/CamaraController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CamaraController : MonoBehaviour
 {
+    private GameObject player;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("Player");
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
