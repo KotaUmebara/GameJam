e6859f5ef703e08e7f0482d47b370ccc08e2bf52
Umebara <Nakoichi39@outlook.jp>
2023-02-08 13:30:59 +0900

    スタート画面の花火にSEを追加

--- Assets/Umebara/ScriptFile/StartFire.cs
+++ Assets/Umebara/ScriptFile/StartFire.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartFire : MonoBehaviour
@@ -7,20 +8,29 @@ public class StartFire : MonoBehaviour
     [SerializeField] GameObject MainFireworks;
     private int angle;
     private float Fcount;
+    private float Ecount;
     private int count;
     private bool fire1;
+    private bool fire2;
+    public AudioClip Explosion;
+    AudioSource audioSource;
     void Start()
     {
         angle = 270;
         Fcount = 0.0f;
+        Fcount = 0.0f;
         count = 0;
         fire1 = false;
+        fire2 = false;
+        //Component‚ðŽæ“¾
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Fcount += Time.deltaTime;
+        Ecount += Time.deltaTime;
         if (Fcount >= 1.0f)
         {
             count++;
@@ -30,11 +40,18 @@ public class StartFire : MonoBehaviour
         {
             count = 0;
             fire1 = false;
+            fire2 = false;
+            Ecount = 0.0f;
         }
         if (count / 5 == 1 && fire1 == false)
         {
             Instantiate(MainFireworks, this.transform.position, Quaternion.Euler(angle, 0, 0));
             fire1 = true;
+        }
+        if (Ecount >= 4.5f && fire2 == false)
+        {
+            audioSource.PlayOneShot(Explosion);
+            fire2 = true;
         }
     }
 }
